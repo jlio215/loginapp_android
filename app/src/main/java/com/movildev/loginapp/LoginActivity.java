f6a436223cd32c16f5784a407af84ef5719ae680
x <jlio215@hotmail.com>
2024-04-20 14:50:24 -0500

    log in ready

--- app/src/main/java/com/movildev/loginapp/LoginActivity.java
+++ app/src/main/java/com/movildev/loginapp/LoginActivity.java
@@ -1,18 +1,22 @@
 package com.movildev.loginapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
 
     private String TAG = "LoginActivity";
-    private EditText username , password;
+    private EditText username , password, email, phone;
     private Button btnsignin;
 
     @Override
@@ -25,8 +29,25 @@ public class LoginActivity extends AppCompatActivity {
         btnsignin = findViewById(R.id.buttonsignin);
     }
 
-    public void SignIn (View v){
+    public void SignIn (View view){
+        String name = username.getText().toString();
+        String pass = password.getText().toString();
+        try {
+            List<User> users = User.find(User.class,"username=?", name);
+            User user = users.get(0);
+            if (user.getPassword().equals(pass)) {
+                Intent intent = new Intent(LoginActivity.this, SplashActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                // Contraseña incorrecta
+                Toast.makeText(LoginActivity.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+            }
+        }catch (Exception e){
+            Log.d(TAG,"login error:"+e);
+        }
 
     }
 
 }
+
